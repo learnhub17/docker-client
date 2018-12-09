dc93c1d6d1ea47cf2e05d519ed0a55b793e1c15e
learner <11056257+learnhub17@users.noreply.github.com>
2018-12-09 18:50:42 +0530

    task schedule by appending log
    get verify with:
    get-content c:\users\anand\desktop\log.txt -wait

--- task-schedule.ps1
+++ task-schedule.ps1
@@ -1,4 +1,3 @@
-$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '“C:\Users\anand\Desktop\test.ps1"'
+$action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "C:\Users\anand\Desktop\test.ps1 >>C:\Users\anand\Desktop\log.txt"
 $trigger =  New-ScheduledTaskTrigger -Daily -At 02:26:20am
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "AppLog" -Description "Daily dump of Applog"
-#Register-ScheduledJob –Name ”Monitor Group Management” -Task "abc"
