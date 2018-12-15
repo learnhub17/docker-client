5bbd6b3c23c1660012773e8e36a82fecd3cd40d2
learner <11056257+learnhub17@users.noreply.github.com>
2018-12-15 18:02:42 +0530

    updated

--- taskschedule.ps1
+++ taskschedule.ps1
@@ -2,7 +2,6 @@ $jobname = "dockerrun"
 $description = "check docker containers daily every 5 minutes"
 $script = "$HOME\appdata\docker-client\dockerrun.ps1"
 $logout = "$HOME\appdata\docker-client\logs\log.txt"
-
 $repeat = (New-TimeSpan -Minutes 5)
 $duration = ([timespan]::FromDays(1000))
 
