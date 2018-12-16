64d1aaca58a60d927c64b234dc01d113d2b841e7
learnhub17 <learnhub17>
2018-12-16 22:13:11 +0530

    added disable pop-up of PS from taskscheduler

--- taskschedule.ps1
+++ taskschedule.ps1
@@ -2,9 +2,10 @@ $jobname = "dockerrun"
 $description = "check docker containers daily every 5 minutes"
 $script = "$HOME\appdata\docker-client\dockerrun.ps1"
 $logout = "$HOME\appdata\docker-client\logs\log.txt"
-$repeat = (New-TimeSpan -Minutes 5)
+$repeat = (New-TimeSpan -Minutes 1)
 $duration = ([timespan]::FromDays(1000))
+$principal = New-ScheduledTaskPrincipal -userID "$env:USERDOMAIN\$env:USERNAME" -LogonType S4U
 
 $action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "$script >>$logout"
 $trigger = New-ScheduledTaskTrigger -once -at (get-date).Date -RepetitionInterval $repeat  -RepetitionDuration $duration
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "$jobname" -Description "$description"
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "$jobname" -Description "$description" -Principal $principal
