19844ca204f985c2b37acc5fdc9d7fdadbedeefa
learner <11056257+learnhub17@users.noreply.github.com>
2018-12-20 15:51:00 +0530

    config taskschedule pull images start containers

--- init.ps1
+++ init.ps1
@@ -1,4 +1,6 @@
 powershell.exe $HOME\appdata\docker-client\env.ps1 $args[0]
 powershell.exe $HOME\appdata\docker-client\taskschedule.ps1
+
 docker pull mongo:4.0.4
 docker pull sumitanand17/test:v1
+powershell.exe $HOME\appdata\docker-client\dockerrun.ps1
