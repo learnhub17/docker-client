a3325df3ee48ca5293ac9e21e2076c503946ce63
learner <11056257+learnhub17@users.noreply.github.com>
2018-12-26 18:42:10 +0530

    updated for old and new version env

--- env.ps1
+++ env.ps1
@@ -2,3 +2,10 @@ $dir=$args[0]
 [Environment]::SetEnvironmentVariable("clientpath", "$dir", "user")
 $data=[environment]::GetEnvironmentVariable('clientpath', 'user')
 
+$dir=$args[1]
+[Environment]::SetEnvironmentVariable("clientversion", "$dir", "user")
+$newversion=[environment]::GetEnvironmentVariable('clientversion', 'user')
+
+$dir=$args[2]
+[Environment]::SetEnvironmentVariable("clientversion", "$dir", "user")
+$oldversion=[environment]::GetEnvironmentVariable('clientversion', 'user')
