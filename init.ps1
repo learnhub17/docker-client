5147465136ad94158d8d72d1cc0f36c3a9d21689
learner <11056257+learnhub17@users.noreply.github.com>
2018-12-26 18:43:46 +0530

    updated for 3rd argument

--- init.ps1
+++ init.ps1
@@ -1,6 +1,6 @@
-powershell.exe $HOME\appdata\docker-client\env.ps1 $args[0]
-powershell.exe $HOME\appdata\docker-client\taskschedule.ps1
+powershell.exe $HOME\appdata\docker-client\env.ps1 $args[0] $args[1] $args[2]
+#powershell.exe $HOME\appdata\docker-client\taskschedule.ps1
 
-docker pull mongo:4.0.4
-docker pull sumitanand17/test:v1
-powershell.exe $HOME\appdata\docker-client\dockerrun.ps1
+#docker pull mongo:4.0.4
+#docker pull sumitanand17/test:v1
+#powershell.exe $HOME\appdata\docker-client\dockerrun.ps1
