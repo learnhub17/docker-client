196a9a290db4f771db4a710340c6a7b2ff1344ba
learner <11056257+learnhub17@users.noreply.github.com>
2019-01-04 20:07:22 +0530

    updated

--- env.ps1
+++ env.ps1
@@ -6,6 +6,15 @@ $dir=$args[1]
 [Environment]::SetEnvironmentVariable("clientversion", "$dir", "user")
 $newversion=[environment]::GetEnvironmentVariable('clientversion', 'user')
 
+if ($args.Count -eq 3) 
+{ echo "argument 1 is:"$args[0]
+  echo "argument 2 is:"$args[1]
+  echo "argument 3 is:"$args[2]
+
 $dir=$args[2]
-[Environment]::SetEnvironmentVariable("clientversion", "$dir", "user")
-$oldversion=[environment]::GetEnvironmentVariable('clientversion', 'user')
+[Environment]::SetEnvironmentVariable("oldclientversion", "$dir", "user")
+$oldversion=[environment]::GetEnvironmentVariable('oldclientversion', 'user') 
+echo $oldversion
+}
+else
+{echo "number of arg received is:"$args.Count}
