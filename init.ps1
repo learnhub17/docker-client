4d39aadb623712fa5ab280c2cf74ef988af809f5
learner <11056257+learnhub17@users.noreply.github.com>
2019-01-04 20:08:00 +0530

    updated

--- init.ps1
+++ init.ps1
@@ -1,6 +1,10 @@
-powershell.exe $HOME\appdata\docker-client\env.ps1 $args[0] $args[1] $args[2]
-#powershell.exe $HOME\appdata\docker-client\taskschedule.ps1
+powershell.exe $HOME\appdata\docker-client\env.ps1 $args[0] $args[1]
+powershell.exe $HOME\appdata\docker-client\taskschedule.ps1
 
-#docker pull mongo:4.0.4
-#docker pull sumitanand17/test:v1
-#powershell.exe $HOME\appdata\docker-client\dockerrun.ps1
+$newversion=[environment]::GetEnvironmentVariable('clientversion', 'user')
+$oldversion=[environment]::GetEnvironmentVariable('oldclientversion', 'user')
+
+echo $newversion
+docker pull mongo:4.0.4
+docker pull original4sure/daemon-client:$newversion
+powershell.exe $HOME\appdata\docker-client\dockerrun.ps1
