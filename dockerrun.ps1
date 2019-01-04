11f503e35b4b0fc245ca5950da9d92201ec6c41b
learner <11056257+learnhub17@users.noreply.github.com>
2019-01-04 20:06:35 +0530

    updated

--- dockerrun.ps1
+++ dockerrun.ps1
@@ -21,21 +21,21 @@ else { echo "Mongo conatiner exist, going to check mongo service" }
 
 if ($mongo -ne 0) 
 { echo "Mongo is not running, going to start Mongo !!!!"
-docker ps  
 docker start mongo
+docker ps  
 }
 else { echo "Mongo is running" }
 start-sleep -s 5
 
 if ($clientservice -ne 0) 
 { echo "daemon-client container is not running, going to start daemon-client conatiner !!!!"
-docker run -idt --name daemon-client -v ${data}:/data --link mongo:mongo --restart=unless-stopped -p 7000:7000 original4sure/daemon-client:$version 
+docker run -idt --name daemon-client -v ${data}:/data --link mongo:mongo --restart=unless-stopped -p 7000:7000 original4sure/daemon-client:$newversion 
 }
 else { echo "daemon-client conatiner exist, going to check daemon-client is service" }
 
 if ($client -ne 0) 
 { echo "daemon-client is not running, going to start daemon-client !!!!"
-docker ps  
 docker start daemon-client
+docker ps 
 }
 else { echo "daemon-client is running" }
