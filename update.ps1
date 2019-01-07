6e182a408743361eb1e0c79a2d585fac118fa60e
learner <11056257+learnhub17@users.noreply.github.com>
2019-01-07 19:43:09 +0530

    updated

--- update.ps1
+++ update.ps1
@@ -1,28 +1,16 @@
-#$ErrorActionPreference = "Stop"
-#$PSDefaultParameterValues['*:ErrorAction']='Stop'
+$data=[environment]::GetEnvironmentVariable('clientpath', 'user')
+$newversion=[environment]::GetEnvironmentVariable('clientversion', 'user')
 
 $logout = "$HOME\appdata\docker-client\logs\update.json"
-powershell.exe $HOME\appdata\docker-client\env.ps1 $args[0] $args[1] $args[2]
-
-$newversion=[environment]::GetEnvironmentVariable('clientversion', 'user')
-$oldversion=[environment]::GetEnvironmentVariable('oldclientversion', 'user')
-$data=[environment]::GetEnvironmentVariable('clientpath', 'user')
-
+powershell.exe $HOME\appdata\docker-client\env.ps1 $args[0] $args[1]
 echo "new version $newversion"
-echo "old version $oldversion"
+echo "new version $data" 
 
 docker ps | findstr daemon-client
 $clientservice=$LASTEXITCODE
 
-#$a = '"'+ $newversion +'"'
-#echo "{" >$logout
-#docker inspect daemon-client | Select-string "original4sure/daemon-client" >>$logout
-#echo '"newversion":'"$a," >>$logout
-#echo '"status":"Started"' >>$logout
-#echo "}" >>$logout
-
 if ($clientservice -eq 0) 
-{ 
+ { 
 
     echo "daemon-client container is running, going to stop remove and pull new version $version !!!!"
 
@@ -33,16 +21,16 @@ if ($clientservice -eq 0)
     echo '"status":"Started"' >>$logout
     echo "}" >>$logout
 
-     docker pull original4sure/daemon-client:$newversion
-     docker ps
-     echo "stopping daemon-client"
-     docker stop daemon-client
-     echo "removing daemon-client"
-     docker rm daemon-client
-     docker run -idt --name daemon-client -v ${data}:/data --link mongo:mongo --restart=unless-stopped -p 7000:7000 original4sure/daemon-client:$newversion 
-     echo "deleting original4sure/daemon-client:$oldversion"
-     docker rmi -f original4sure/daemon-client:$oldversion
-     docker ps
+    docker pull original4sure/daemon-client:$newversion
+    docker ps
+    echo "stopping daemon-client"
+    docker stop daemon-client
+    echo "removing daemon-client"
+    docker rm daemon-client
+    docker run -idt --name daemon-client -v ${data}:/data --link mongo:mongo --restart=unless-stopped -p 7000:7000 original4sure/daemon-client:$newversion 
+    echo "deleting original4sure/daemon-client:$oldversion"
+    docker rmi -f original4sure/daemon-client:$oldversion
+    docker ps
 
     $a = '"'+ $newversion +'"'
     echo "{" >$logout
@@ -50,10 +38,10 @@ if ($clientservice -eq 0)
     echo '"newversion":'"$a," >>$logout
     echo '"status":"done"' >>$logout
     echo "}" >>$logout
-    } 
+  } 
 else { echo " daemon-client is not runing !!!!" 
-# docker ps
-# docker rm daemon-client
-# docker run -idt --name daemon-client -v ${data}:/data --link mongo:mongo --restart=unless-stopped -p 7000:7000 original4sure/daemon-client:$newversion
+ docker ps
+ docker rm daemon-client
+ docker run -idt --name daemon-client -v ${data}:/data --link mongo:mongo --restart=unless-stopped -p 7000:7000 original4sure/daemon-client:$newversion
  docker ps
 }
